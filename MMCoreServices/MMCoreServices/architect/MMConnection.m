08f09c4f4262900cde2ebdfba2cfd795bcc8b2f3
wangcnt <404298011@qq.com>
2018-02-05 00:11:48 +0800

    gitignore

--- MMCoreServices/MMCoreServices/architect/MMConnection.m
+++ MMCoreServices/MMCoreServices/architect/MMConnection.m
@@ -59,7 +59,6 @@ typedef enum : NSUInteger {
     self = [super init];
     if (self) {
         _semaphore = dispatch_semaphore_create(1);
-        
         dispatch_queue_t delegate_queue = dispatch_queue_create("com.markwong.mmcoreservices.connection.socket.delegatequeue", DISPATCH_QUEUE_SERIAL);
         _socket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:delegate_queue];
         
