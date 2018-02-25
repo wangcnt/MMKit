34166794c492e785a9bbfd5f9f40d15fefe3fe1a
wangcnt <404298011@qq.com>
2018-02-26 00:48:04 +0800

    rename methods

--- MMArchitecture/MMArchitecture/MMSessionManager.m
+++ MMArchitecture/MMArchitecture/MMSessionManager.m
@@ -139,7 +139,7 @@
     return connection;
 }
 
-- (NSString *)anyIdentifierForOffworkedConnectionAndCreateIfNeeds {
+- (NSString *)identifierForAnyOffworkedConnection {
     return [self anyOffworkedConnection].identifier;
 }
 
