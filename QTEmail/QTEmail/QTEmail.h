eca1a1a0395372925109433bdf518068d7ac7b10
wangcnt <404298011@qq.com>
2018-02-26 20:01:15 +0800

    QTEmail, QTime

---
+++ QTEmail/QTEmail/QTEmail.h
@@ -0,0 +1,19 @@
+//
+//  QTEmail.h
+//  QTEmail
+//
+//  Created by Mark on 2018/2/26.
+//  Copyright © 2018年 Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QTEmail.
+FOUNDATION_EXPORT double QTEmailVersionNumber;
+
+//! Project version string for QTEmail.
+FOUNDATION_EXPORT const unsigned char QTEmailVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QTEmail/PublicHeader.h>
+
+
