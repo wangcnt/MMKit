eee991016e269939c1310d6695b444e120267c9a
wangcnt <404298011@qq.com>
2018-03-04 23:10:45 +0800

    1. MMCoreServices.關聯關係；2. MMSecurityKit.framework

---
+++ MMSecurityKit/MMSecurityKit/MMSecurityKit.h
@@ -0,0 +1,19 @@
+//
+//  MMSecurityKit.h
+//  MMSecurityKit
+//
+//  Created by Mark on 2018/3/4.
+//  Copyright © 2018年 Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MMSecurityKit.
+FOUNDATION_EXPORT double MMSecurityKitVersionNumber;
+
+//! Project version string for MMSecurityKit.
+FOUNDATION_EXPORT const unsigned char MMSecurityKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMSecurityKit/PublicHeader.h>
+
+
