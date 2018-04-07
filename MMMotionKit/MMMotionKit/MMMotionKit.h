60c692571b789bc947dfe3dcac0ea18ae9c79223
wangcnt <404298011@qq.com>
2018-04-07 21:51:23 +0800

    MMMotionKit

---
+++ MMMotionKit/MMMotionKit/MMMotionKit.h
@@ -0,0 +1,22 @@
+//
+//  MMMotionKit.h
+//  MMMotionKit
+//
+//  Created by Mark on 2018/4/7.
+//  Copyright © 2018年 Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MMMotionKit.
+FOUNDATION_EXPORT double MMMotionKitVersionNumber;
+
+//! Project version string for MMMotionKit.
+FOUNDATION_EXPORT const unsigned char MMMotionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMMotionKit/PublicHeader.h>
+
+#import <MMMotionKit/MMMotionDetector.h>
+#import <MMMotionKit/MMLocationManager.h>
+#import <MMMotionKit/MMWtepDetector.h>
+
