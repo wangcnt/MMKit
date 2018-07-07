c4ee3d68fddc9e9f1b4119393956e57f78c5048f
wangcnt <404298011@qq.com>
2018-07-07 20:25:28 +0800

    插件化实例

--- MMRuntime/MMRuntime/MMBundleProvider.m
+++ MMRuntime/MMRuntime/MMBundleProvider.m
@@ -8,41 +8,87 @@
 
 #import "MMBundleProvider.h"
 
+#import <MMFoundation/MMDefines.h>
+#import "MMBundleDelegate.h"
+#import "MMURI.h"
+#import "MMBundle.h"
+
+@interface MMBundleManager ()
+@property (nonatomic, strong) NSMutableDictionary<NSString *, MMBundle *> *bundles; ///< 注册成功的bundle
+@end
+
 @implementation MMBundleManager
 
-- (MMBundle *)bundleDelegateForURI:(MMURI *)uri {
-    return nil;
+__singleton__(MMBundleManager)
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _bundles = [NSMutableDictionary<NSString *, MMBundle *> dictionary];
+    }
+    return self;
 }
 
-- (void)installBundleWithURI:(MMURI *)uri {
-    
+- (id<MMBundleDelegate>)bundleDelegateForIdentifier:(NSString *)identifier {
+    return _bundles[identifier].principalDelegate;
+}
+
+- (BOOL)installBundleWithIdentifier:(NSString *)identifier frameworkName:(NSString *)frameworkName {
+    BOOL successed = NO;
+    if(identifier.length && frameworkName.length) {
+        NSString *principalClassString = [NSString stringWithFormat:@"%@BundleDelegate", frameworkName];
+        Class clazz = NSClassFromString(principalClassString);
+        if(clazz) {
+            id<MMBundleDelegate> bundleDelegate = [[clazz alloc] init];
+            if(bundleDelegate) {
+                MMBundle *bundle = [[MMBundle alloc] init];
+                bundle.identifier = identifier;
+                bundle.frameworkName = frameworkName;
+                bundle.principalDelegate = bundleDelegate;
+                _bundles[identifier] = bundle;
+                successed = YES;
+            }
+        }
+    }
+    if(!successed) {
+        NSLog(@"Framework %@ with identifier %@ install failed.", frameworkName, identifier);
+    }
+    return successed;
 }
 
 - (void)installEmbeddedBundles {
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"configuration" ofType:@"plist"];
+    NSDictionary *configuration = [NSDictionary dictionaryWithContentsOfFile:path];
+    NSString *identifier = nil;
+    NSString *frameworkName = nil;
+    for(NSDictionary *item in configuration.allValues) {
+        identifier = item[@"identifier"];
+        frameworkName = item[@"frameworkName"];
+        [self installBundleWithIdentifier:identifier frameworkName:frameworkName];
+    }
+}
+
+- (void)installDownloadedBundleWithIdentifier:(NSString *)identifier completion:(void (^)(NSError *))completion {
     
 }
 
-- (void)installDownloadedBundleWithURI:(MMURI *)uri completion:(void (^)(NSError *error))completion {
-    
-}
-
-- (MMBundle *)bundleWithURI:(MMURI *)uri {
-    return nil;
+- (MMBundle *)bundleWithIdentifier:(NSString *)identifier {
+    return _bundles[identifier];
 }
 
 - (NSArray<MMBundle *> *)bundles {
-    
+    return _bundles.allValues;
 }
 
-- (BOOL)bundleIsInstalledForIdentifier:(MMURI *)uri {
-    
+- (BOOL)bundleIsInstalledForIdentifier:(NSString *)identifier {
+    return _bundles[identifier] != nil;
 }
 
 - (void)analyticsUsageUsingProgress:(void (^)(MMBundle *bundle, unsigned long long totalSize, unsigned long long analyzedSize))progress withCompletion:(void (^)(MMBundle *bundle))completion {
     
 }
 
-- (void)clearBundleStorageWithURI:(MMURI *)uri {
+- (void)clearBundleStorageWithIdentifier:(NSString *)identifier {
     
 }
 
